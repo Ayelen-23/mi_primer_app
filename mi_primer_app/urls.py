e2269b54a5bc7c4c8c5af37d933b6ff82e9e7dcd
Ayelen Roberts <aye_roberts@hotmail.com>
2025-07-06 23:35:13 -0300

    se configura herencia de html

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from.views import saludo, saludo_con_templates
+from.views import saludo, saludo_con_templates, inicio
 
 urlpatterns = [
+    path('', inicio , name='inicio'),
     path('Hola-mundo/', saludo),
     path('Hola-mundo-templates/', saludo_con_templates),
     
--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse 
 
+def inicio(request):
+    return render(request,'mi_primer_app/inicio.html')
+
 def saludo(request):
     return HttpResponse("Hola mundo")
 
